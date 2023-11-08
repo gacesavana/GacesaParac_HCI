964d097bc158ee425ceab067121224ac502f8395
Sparac1203 <sparac00@fesb.hr>
2023-11-08 20:31:14 +0100

    class project 4 DONE

--- class-projects/class-project-2/nextjs/gacesa-parac-app/src/app/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return (
-    <h1 className="flex justify-center p-14 text-4xl">Blog page</h1>
-  )
-}
--- semester-project/gacesa-parac-app/src/app/layout.tsx
+++ semester-project/gacesa-parac-app/src/app/layout.tsx
@@ -23,13 +23,17 @@ export default function RootLayout({
             <li><Link href="/" className="text-white hover:bg-mint-100 p-4 hover:text-black">Home</Link></li>
             <li><Link href='/shop' className="text-white hover:underline">Shop</Link></li>
             <li><Link href="/aboutUs" className="text-white hover:underline">About Us</Link></li>
-            <li><Link href="/blog" className="text-white hover:underline">Blog</Link></li>
+            <li><Link href="/blog?_page=1&_limit=10" className="text-white hover:underline">Blog</Link></li>
             <li><Link href="/signIn" className="text-white hover:underline">Sign In</Link></li>
           </ul>
         </nav>
 
 
-        {children}</body>
+        {children}
+        {/* <footer>
+          <p className='centered-content'>Vana Gaćeša & Šime Parać</p>
+          <p className='centered-content'>2023 FESB</p>
+        </footer> */}</body>
     </html>
   )
 }
